0d68d28d4b5a522677f1af2d7c23639fbea783f9
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-26 17:36:05 -0500

    Create routes to Home and BookList components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import BookList from './components/book/BookList';
-import './App.css';
 import Home from './components/home/Home';
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/books" component={BookList} />
+      </Switch>
     </main>
   );
 }
