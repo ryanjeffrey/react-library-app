54c6c4123b93f4fef0185573dd1fe8c098c1aa9d
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-26 17:26:28 -0500

    Create Home component and add it to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import BookList from './components/book/BookList';
 import './App.css';
+import Home from './components/home/Home';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Home />
     </main>
   );
 }
