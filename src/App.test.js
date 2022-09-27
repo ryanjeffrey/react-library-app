387e50f12363a4d4e9e9cd8751ad702ebd72c192
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-26 19:35:43 -0500

    Update test to pass CI (part deux)

--- src/App.test.js
+++ src/App.test.js
@@ -9,5 +9,5 @@ it('renders a welcome message when signed out', async () => {
     </MemoryRouter>
   );
 
-  await screen.findByText('list', { name: 'Library Catalog' });
+  await screen.findByText('Library Catalog');
 });
